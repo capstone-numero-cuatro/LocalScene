e1ee56c29edcc8bdd4d2c6124eb294308108630a
Paul Garcia <paul.ngarcia@yahoo.com>
2023-06-01 21:13:43 -0500

    initial

---
+++ src/test/java/com/example/localscene/LocalSceneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localscene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalSceneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
