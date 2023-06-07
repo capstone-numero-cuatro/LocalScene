3e6c90d4f3ace1b050f8879832dde05055908729
Jaewoo Baek <jaewoo.baek01@gmail.com>
2023-06-07 12:02:57 -0500

    venues card updated

--- src/main/resources/static/js/keyTM.js
+++ src/main/resources/static/js/keyTM.js
@@ -1,13 +1,4 @@
 
-// function searchVenues(location) {
-//     let options = {
-//         keyword: 'music',
-//         locale: 'en-us',
-//         size: 10,
-//         radius: 100 // Set the radius in miles (adjust as needed)
-//     };
-//
-
     function fetchVenues(keyword) {
         var baseUrl = 'https://app.ticketmaster.com';
         var endPoint = '/discovery/v2/venues';
@@ -19,51 +10,56 @@
                 return response.json();
             }).then(function (data) {
                 console.log(data);
+                displayVenues(data);
             });
     }
 
+    function displayVenues(data) {
+        var venuesContainer = document.getElementById('venues-container');
+        venuesContainer.innerHTML = ''; // Clear previous content
 
+        var venues = data._embedded.venues;
+        venues.forEach(function (venue) {
+            var venueCard = createVenueCard(venue);
+            venuesContainer.appendChild(venueCard);
+        });
+    }
 
-// function searchVenues(location) {
-//     let options = {
-//         keyword: 'music',
-//         locale: 'en-us',
-//         size: 10,
-//         city: location,
-//         radius: 100 // Set the radius in miles (adjust as needed)
-//     };
-// // TMConnect=
-// TMConnect.venuesDiscovery.search(options)
-//     .then(function(response) {
-//         displayVenues(response.embedded.venues);
-//     })
-//     .catch(function(error) {
-//         console.log('Error:', error);
-//     });
-// }
-//
-// // Function to display the venues
-//     function displayVenues(venues) {
-//         let container = $('#venues-container');
-//         container.empty();
-//
-//         $.each(venues, function (index, venue) {
-//             let venueCard = $('<div>').addClass('col-md-4');
-//             let card = $('<div>').addClass('card venue-card');
-//             let cardBody = $('<div>').addClass('card-body');
-//             let title = $('<h5>').addClass('card-title').text(venue.name);
-//             let address = $('<p>').addClass('card-text').text(venue.address.line1);
-//
-//             cardBody.append(title, address);
-//             card.append(cardBody);
-//             venueCard.append(card);
-//             container.append(venueCard);
-//         });
-//     }
-//
-// // Handle form submission
-//     $('#search-form').submit(function (event) {
-//         event.preventDefault();
-//         let location = $('#search-input').val();
-//         searchVenues(location);
-//     });
+    function createVenueCard(venue) {
+        var card = document.createElement('div');
+        card.classList.add('card', 'col-md-6', 'mb-4');
+
+        var cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+        var name = document.createElement('h5');
+        name.classList.add('card-title');
+        name.textContent = venue.name;
+
+        var address = document.createElement('p');
+        address.classList.add('card-text');
+        address.textContent = venue.address.line1 + ', '+ venue.city.name + ', ' + venue.state.name + ', ' + venue.postalCode;
+
+        var description = document.createElement('p');
+        description.classList.add('card-text');
+        description.textContent = venue.description;
+
+        // var image = document.createElement('img');
+        // image.classList.add('card-img-top');
+        // image.src = venue.images[0].url;
+        // image.alt = venue.name;
+
+        cardBody.appendChild(name);
+        cardBody.appendChild(address);
+        cardBody.appendChild(description);
+        // card.appendChild(image);
+        card.appendChild(cardBody);
+
+        return card;
+    }
+
+    document.getElementById('search-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        var keyword = document.getElementById('search-input').value;
+        fetchVenues(keyword);
+    });
