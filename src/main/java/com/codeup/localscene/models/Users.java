a7cffd08de77000fad19010f6e286acf48c52d23
bcopy  ~/.ssh/id_rsa.pub <ryan.zhang1120@gmail.com>
2023-06-09 13:17:52 -0700

    Users database update

--- src/main/java/com/codeup/localscene/models/Users.java
+++ src/main/java/com/codeup/localscene/models/Users.java
@@ -31,28 +31,8 @@ public class Users {
     @Column()
     private boolean enabled;
 
-    @Column(length = 45)
-    private String instrument;
-
-    @Column
-    private boolean band_member;
-
-    @Column()
-    private long user_image;
-
-    @Column()
-    private String instagram;
-
-    @Column()
-    private String facebook;
-
-    @Column()
-    private String twitter;
-
-
-    @Transient
-    private String confirmPassword;
-
+    @Column(name = "profile_image", length = 500)
+    private String profileImage;
 
     public Users(Users copy) {
         id = copy.id;
@@ -61,12 +41,7 @@ public class Users {
         verificationCode = copy.verificationCode;
         resetPasswordToken = copy.resetPasswordToken;
         enabled = copy.enabled;
-        instrument = copy.instrument;
-        band_member = copy.band_member;
-        user_image = copy.user_image;
-        instagram = copy.instagram;
-        facebook = copy.facebook;
-        twitter = copy.twitter;
+        profileImage = copy.profileImage;
     }
 
     public Users (){
@@ -96,14 +71,6 @@ public class Users {
         this.password = password;
     }
 
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
     public String getEmail() {
         return email;
     }
@@ -128,21 +95,6 @@ public class Users {
         this.enabled = enabled;
     }
 
-    public String getInstrument() {
-        return instrument;
-    }
-
-    public void setInstrument(String instrument) {
-        this.instrument = instrument;
-    }
-
-    public boolean isBand_member() {
-        return band_member;
-    }
-
-    public void setBand_member(boolean band_member) {
-        this.band_member = band_member;
-    }
 
     public String getResetPasswordToken() {
         return resetPasswordToken;
@@ -152,35 +104,12 @@ public class Users {
         this.resetPasswordToken = resetPasswordToken;
     }
 
-    public String getInstagram() {
-        return instagram;
+    public String getProfileImage() {
+        return profileImage;
     }
 
-    public void setInstagram(String instagram) {
-        this.instagram = instagram;
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
     }
 
-    public String getFacebook() {
-        return facebook;
-    }
-
-    public void setFacebook(String facebook) {
-        this.facebook = facebook;
-    }
-
-    public String getTwitter() {
-        return twitter;
-    }
-
-    public void setTwitter(String twitter) {
-        this.twitter = twitter;
-    }
-
-    public long getUser_image() {
-        return user_image;
-    }
-
-    public void setUser_image(long user_image) {
-        this.user_image = user_image;
-    }
 }
--- src/main/resources/static/js/keys.js
+++ src/main/resources/static/js/keys.js
@@ -1,5 +1,6 @@
 let myToken ="pk.eyJ1Ijoid29kbjIzNDUiLCJhIjoiY2xpajJ1aTA2MDNyejNlcGJ1NTJ5ZTNnYSJ9.U7kLp4bsqObbZ2DHI7b63w";
 let apiKey = 'UdZ8l4XkAoz0AEQZV9sRX2cOqvZL1Atw';
+let filestackKey ="Abd5534iTviZyWy5jGpswz";
 
 
 $('#cityLocation').click(function(e){
