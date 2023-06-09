f420962483edbe7da9a32b4e57c50233af17c1c9
Paul Garcia <paul.ngarcia@yahoo.com>
2023-06-09 16:32:33 -0500

    got posts to appear on home page

---
+++ src/main/java/com/codeup/localscene/controllers/HomeController.java
@@ -0,0 +1,44 @@
+package com.codeup.localscene.controllers;
+import com.codeup.localscene.models.Posts;
+import com.codeup.localscene.models.Users;
+import com.codeup.localscene.repositories.PostRepository;
+import com.codeup.localscene.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    public HomeController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    private PostRepository postRepository;
+
+    @GetMapping("/home")
+
+    public String welcome(Model model){
+    List<Posts> posts =postRepository.findAll();
+        model.addAttribute("posts", posts);
+
+    return"home";}
+
+//    @GetMapping("/posts")
+//    public String showPosts(Model model){
+//        List<Posts> posts =postRepository.findAll();
+//        model.addAttribute("posts", posts);
+//
+//        return "posts";
+//
+//    }
+
+
+}
