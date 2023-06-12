ff0e0c6db6c4fc359a160f5c7c876258f318ac5a
lillian-jewel <lillian.j.arcos@gmail.com>
2023-06-12 09:06:35 -0500

    PostController

--- src/main/java/com/codeup/localscene/controllers/PostController.java
+++ src/main/java/com/codeup/localscene/controllers/PostController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
+import java.util.List;git
 
 @Controller
 public class PostController {
