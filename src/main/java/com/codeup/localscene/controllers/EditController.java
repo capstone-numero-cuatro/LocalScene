198dc1112cdd18bcc61f81a585883905e4776c46
bcopy  ~/.ssh/id_rsa.pub <ryan.zhang1120@gmail.com>
2023-06-12 14:43:30 -0700

    rest password in edit

--- src/main/java/com/codeup/localscene/controllers/EditController.java
+++ src/main/java/com/codeup/localscene/controllers/EditController.java
@@ -1,5 +1,6 @@
 package com.codeup.localscene.controllers;
 
+import com.codeup.localscene.models.PasswordResetForm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -7,10 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.ui.Model;
 import com.codeup.localscene.models.Users;
-import com.codeup.localscene.repositories.BandRepository;
-import com.codeup.localscene.repositories.PostRepository;
 import com.codeup.localscene.repositories.UserRepository;
-import com.codeup.localscene.services.EmailService;
 import com.codeup.localscene.services.UserDetailsLoader;
 import com.codeup.localscene.services.PasswordResetService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,25 +16,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 
 @Controller
 public class EditController {
     private final UserRepository userRepository;
-    private final PostRepository postRepository;
-    private final BandRepository bandRepository;
     private final PasswordResetService passwordResetService;
-    private final EmailService emailService;
     private final UserDetailsLoader userService;
 
     @Autowired
-    public EditController(UserRepository userRepository, PostRepository postRepository, BandRepository bandRepository, PasswordResetService passwordResetService, EmailService emailService, UserDetailsLoader userService) {
+    public EditController(UserRepository userRepository, PasswordResetService passwordResetService, UserDetailsLoader userService) {
         this.userRepository = userRepository;
-        this.postRepository = postRepository;
-        this.bandRepository = bandRepository;
         this.passwordResetService = passwordResetService;
-        this.emailService = emailService;
         this.userService = userService;
     }
     @Value("${filestack.api.key}")
@@ -47,6 +40,8 @@ public class EditController {
         Users user = userRepository.findByUsername(username);
         model.addAttribute("user", user);
         model.addAttribute("filestackKey", filestackApiKey);
+        model.addAttribute("passwordResetForm", new PasswordResetForm());
+
         return "/users/edit-profile";
     }
 
@@ -67,7 +62,7 @@ public class EditController {
                 Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
                 SecurityContextHolder.getContext().setAuthentication(authentication);
 
-                model.addAttribute("message", "Profile updated successfully!");
+                model.addAttribute("messages", "Profile updated successfully!");
                 return "redirect:/profile/" + currentUser.getId();
             } else {
                 model.addAttribute("error", "Error updating profile.");
@@ -77,5 +72,23 @@ public class EditController {
             model.addAttribute("error", "Error updating profile: " + e.getMessage());
             return "redirect:/profile/edit";
         }
+    }
+    @PostMapping("/profile/edit/reset-password")
+    public String handlePasswordReset(@ModelAttribute("passwordResetForm") PasswordResetForm form, Principal principal, RedirectAttributes redirectAttributes) {
+        String username = principal.getName();
+        Users user = userRepository.findByUsername(username);
+        String email = user.getEmail();
+
+        // check if current password is correct
+        if (!passwordResetService.authenticateUser(email, form.getCurrentPassword())) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Current password is incorrect.");
+            return "redirect:/profile/edit";
+        }
+
+        // update password
+        passwordResetService.updatePassword(user, form.getNewPassword());
+
+        redirectAttributes.addFlashAttribute("message", "Password successfully updated.");
+        return "redirect:/profile/edit";
     }
 }
