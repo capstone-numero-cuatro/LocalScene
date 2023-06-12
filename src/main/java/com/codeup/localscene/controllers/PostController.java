01aa30eecc96e5d9b6437c676e15454e379de25a
lillian-jewel <lillian.j.arcos@gmail.com>
2023-06-12 09:48:05 -0500

    Errors

--- src/main/java/com/codeup/localscene/controllers/PostController.java
+++ src/main/java/com/codeup/localscene/controllers/PostController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;git
+import java.util.List;
 
 @Controller
 public class PostController {
