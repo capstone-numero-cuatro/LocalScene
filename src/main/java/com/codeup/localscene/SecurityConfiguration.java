3d54d352460c6bdacd6e1a118606243ff53a97db
Paul Garcia <paul.ngarcia@yahoo.com>
2023-06-15 15:45:04 -0500

    minor change of sec config line 55 - kh

--- src/main/java/com/codeup/localscene/SecurityConfiguration.java
+++ src/main/java/com/codeup/localscene/SecurityConfiguration.java
@@ -52,7 +52,7 @@ public class SecurityConfiguration {
                                 "/verify", "/home","/forgot-password","/reset-password",
                                 "/band-profile/{bandId}", "/venues", "/events").permitAll()
                         .requestMatchers("/profile/{id}","/profile/edit").authenticated()
-                        .anyRequest().authenticated()
+//                        .anyRequest().authenticated()
                 )
         ;
 
