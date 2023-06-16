63087598a9f79c780d78cf01de6504c417931f16
Paul Garcia <paul.ngarcia@yahoo.com>
2023-06-16 10:08:45 -0500

    trying sec fixes

--- src/main/java/com/codeup/localscene/SecurityConfiguration.java
+++ src/main/java/com/codeup/localscene/SecurityConfiguration.java
@@ -9,8 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -34,45 +33,45 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .formLogin(formLogin -> formLogin
-//                        .loginPage("/login")
-//                        .usernameParameter("email")
-//                        .passwordParameter("password")
-//                        .defaultSuccessUrl("/home", true)
-//                        .failureUrl("/login?error=true")
-//                        .permitAll()
-//                )
-//                .logout(logout -> logout
-//                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                        .logoutSuccessUrl("/home")
-//                )
-//                .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers("/404","/img/**","/js/**", "/css/**", "/", "/sign-up",
-//                                "/verify", "/home","/forgot-password","/reset-password",
-//                                "/band-profile/{bandId}", "/venues", "/events").permitAll()
-//                        .requestMatchers("/profile/{id}","/profile/edit").authenticated()
-////                        .anyRequest().authenticated()
-//                );
-
-        http /* Login configuration */
-                .formLogin((login) -> login.loginPage("/login").defaultSuccessUrl("/home"))
-                /* Logout configuration */
-                .authorizeHttpRequests((requests) -> requests
-                        /* Pages that require authentication
-                         * only authenticated users can create and edit ads */
-                        .requestMatchers("/profile/{id}","/profile/edit").authenticated()
-                        /* Pages that do not require authentication
-                         * anyone can visit the home page, register, login, and view ads */
-                        .requestMatchers("/404", "/", "/sign-up",
+        http
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/home", true)
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/home")
+                )
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/404","/img/**","/js/**", "/css/**", "/", "/sign-up",
                                 "/verify", "/home","/forgot-password","/reset-password",
                                 "/band-profile/{bandId}", "/venues", "/events").permitAll()
-                        // allow loading of static resources
-                        .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
-                )
-                .logout((logout) -> logout.logoutSuccessUrl("/home"))
-                .httpBasic(withDefaults()
+                        .requestMatchers("/profile/{id}","/profile/edit").authenticated()
+//                        .anyRequest().authenticated()
                 );
+//
+//        http /* Login configuration */
+//                .formLogin((login) -> login.loginPage("/login").defaultSuccessUrl("/home"))
+//                /* Logout configuration */
+//                .authorizeHttpRequests((requests) -> requests
+//                        /* Pages that require authentication
+//                         * only authenticated users can create and edit ads */
+//                        .requestMatchers("/profile/{id}","/profile/edit").authenticated()
+//                        /* Pages that do not require authentication
+//                         * anyone can visit the home page, register, login, and view ads */
+//                        .requestMatchers("/404", "/", "/sign-up",
+//                                "/verify", "/home","/forgot-password","/reset-password",
+//                                "/band-profile/{bandId}", "/venues", "/events").permitAll()
+//                        // allow loading of static resources
+//                        .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+//                )
+//                .logout((logout) -> logout.logoutSuccessUrl("/home"))
+//                .httpBasic(withDefaults()
+//                );
 
 
 
